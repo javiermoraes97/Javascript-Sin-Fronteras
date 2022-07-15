ddc4c8deaa8604bb10bf5dd3c72b5f0a0cb17b46
Javier Moraes <javiermoraes97@gmail.com>
2022-07-14 21:40:33 -0500

    Day 11
    14/07/2022 Terminando la aplicacion de To Do List Con Javascript aun falta practicar por mi cuenta

--- 009. TODO List/main.js
+++ 009. TODO List/main.js
@@ -4,30 +4,108 @@
 /* const todos = [];
 
 window.onload = () => {
-    const form = document.getElementById('todo-form'); // Tomamos el formulario y tomamos la referencia con getelement
-    form.onsubmit = (e) => { // Luego reemplazamos la funcion que tiene en onsubmit
-        e.preventDefault(); //elimina el efecto que tienen los formularios que hace que la pagina se refresque
-        const todo = document.getElementById('todo'); //Luego vamos a todo para sacarle el valor que tiene
-        const todoText = todo.value; // para reemplazarlo por un value
-        todo.value = ''; // luego reemplazamos su valor por un string vacio
-        console.log(todo); //Aca mostramos en consola su valor
-    }
+const form = document.getElementById('todo-form'); // Tomamos el formulario y tomamos la referencia con getelement
+form.onsubmit = (e) => { // Luego reemplazamos la funcion que tiene en onsubmit
+  e.preventDefault(); //elimina el efecto que tienen los formularios que hace que la pagina se refresque
+  const todo = document.getElementById('todo'); //Luego vamos a todo para sacarle el valor que tiene
+  const todoText = todo.value; // para reemplazarlo por un value
+  todo.value = ''; // luego reemplazamos su valor por un string vacio
+  console.log(todo); //Aca mostramos en consola su valor
+}
 } */
 
+/* const todos = [];
+
+window.onload = () => {
+const form = document.getElementById('todo-form'); 
+form.onsubmit = (e) => { 
+  e.preventDefault();
+  const todo = document.getElementById('todo'); 
+  const todoText = todo.value; 
+  todo.value = '';
+  todos.push(todoText);
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = '';
+  for(let i = 0; i < todos.length; i++) {
+      todoList.innerHTML += '<li>' + todos[i] + '</li>';
+  }
+}
+} */
+
+
+/* // Ahora con map el cual necesita un valor de retorno
 const todos = [];
 
 window.onload = () => {
-    const form = document.getElementById('todo-form'); 
-    form.onsubmit = (e) => { 
-        e.preventDefault();
-        const todo = document.getElementById('todo'); 
-        const todoText = todo.value; 
-        todo.value = '';
-        todos.push(todoText);
-        const todoList = document.getElementById('todo-list');
-        todoList.innerHTML = '';
-        for(let i = 0; i < todos.length; i++) {
-            todoList.innerHTML += '<li>' + todos[i] + '</li>';
-        }
-    }
+const form = document.getElementById('todo-form'); 
+form.onsubmit = (e) => { 
+  e.preventDefault();
+  const todo = document.getElementById('todo'); 
+  const todoText = todo.value; 
+  todo.value = '';
+  todos.push(todoText);
+  const todoList = document.getElementById('todo-list');
+  const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+  todoList.innerHTML = todosTemplate.join('');
+  const elementos = document.querySelectorAll('#todo-list li');
+  elementos.forEach((elemento, i) => {
+      elemento.addEventListener('click', () => {
+          
+      console.log(elemento,i);
+      })
+  })
+}
+} */
+
+/* // Ahora vamos a eliminar elementos
+const todos = [];
+
+window.onload = () => {
+const form = document.getElementById('todo-form'); 
+form.onsubmit = (e) => { 
+  e.preventDefault();
+  const todo = document.getElementById('todo'); 
+  const todoText = todo.value; 
+  todo.value = '';
+  todos.push(todoText);
+  const todoList = document.getElementById('todo-list');
+  const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+  todoList.innerHTML = todosTemplate.join('');
+  const elementos = document.querySelectorAll('#todo-list li');
+  elementos.forEach((elemento, i) => {
+      elemento.addEventListener('click', () => {
+          elemento.parentNode.removeChild(elemento)
+          todos.splice(i, 1)
+      })
+  })
+}
+} */
+
+
+const todos = []
+
+const render = () =>{
+  const todoList = document.getElementById('todo-list');
+  const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+  todoList.innerHTML = todosTemplate.join('');
+  const elementos = document.querySelectorAll('#todo-list li');
+  elementos.forEach((elemento, i) => {
+      elemento.addEventListener('click', () => {
+          elemento.parentNode.removeChild(elemento)
+          todos.splice(i, 1)
+          render()
+      })
+  })
+}
+
+window.onload = () => {
+  const form = document.getElementById('todo-form'); 
+  form.onsubmit = (e) => { 
+    e.preventDefault();
+    const todo = document.getElementById('todo'); 
+    const todoText = todo.value; 
+    todo.value = '';
+    todos.push(todoText);
+    render()
+  }
 }
--- Javascript.md
+++ Javascript.md
@@ -5,3 +5,9 @@
 - Lo primero es crear una carpeta con el nombre de js y en el crear un archivo de index.js 
 - Cada vez que quieras iniciar el proceso que nosotros hayamos realizado usar el codigo de /<touch index.js>
 - Cada vez que quieras ejecutar algun cambio que nosotros hayamos realizado /<node index.js>
+
+- /<push>
+- /<arr.splice>
+- /<arr.push>
+- /<document.querySelectorAll('#id')>
+- /<localstorage>
